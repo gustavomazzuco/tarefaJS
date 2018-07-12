09c587f48c4cf3da20aade7e22131c0061e7fffa
Mazzuco <Gustavo.Mazzuco@thomsonreuters.com>
2018-07-12 15:03:29 -0300

    Modificação 2 de testeJS

--- testeJS.js
+++ testeJS.js
@@ -1,4 +1,4 @@
-var carro = function (id, nome, marca, cor, acessorios, ano, vencimentoDoc) {
+var carro = function (id, nome, marca, cor, acessorios, preco, ano, dataFabricacao, vencimentoDoc) {
 
     return carro = {
         id: id,
@@ -6,7 +6,9 @@ var carro = function (id, nome, marca, cor, acessorios, ano, vencimentoDoc) {
         marca: marca,
         cor: cor,
         acessorios: acessorios,
+        preco: preco,
         ano: ano,
+        dataFabricacao: dataFabricacao,
         vencimentoDoc: vencimentoDoc
     }
 
@@ -38,8 +40,6 @@ var validadeDocumento = function (dataVencimento){
 
     if (vencimento < hoje){
         console.log("Documento está vencido.");
-    } else if (vencimento === hoje ){
-        console.log("Documento vence hoje.");
     } else {
         console.log("Documento está em dia.");
     }
@@ -58,19 +58,38 @@ var tempoVencimento = function (dataVencimento){
     }
 }
 
-var verificaTetoSolar = function(acessorio){
+var valorDepreciado = function(carro){
+    var hoje = new Date(); 
+    var fabricacao = new Date(carro.dataFabricacao);
+    var totalMeses = (hoje.getFullYear() - fabricacao.getFullYear())*12 + (hoje.getMonth() - fabricacao.getMonth());
+
+    var precoVenda = carro.preco;
+    var precoDepreciado = precoVenda;
+    
+    for(i=0 ; i < totalMeses ; i++){
+        precoDepreciado = precoDepreciado * 0.9834;
+    }
+
+    console.log(totalMeses)
+    console.log("Preço de venda do carro: R$" + precoVenda);
+    console.log("Preço depreciado: R$" + precoDepreciado);
+}
+
+var verificaAcessorio = function(acessorio){
     var carroAcessorio = carro.acessorios.filter(function (elemento){
         return elemento === acessorio;
-    });    
+    });
+
+    if (carroAcessorio.length > 0){
+        console.log("Este carro possui " + acessorio);
+    } else {
+        console.log("Este carro não possui " + acessorio);
+    }
 };
 
-///var carroAcessorio = carro.acessorios.filter(function (elemento){
-//    return elemento === "automatico";
-//});
-
-console.log(carro(1, "Ka", "Ford", "Vermelho", ["vidro eletrico", "Teto Solar", "automatico"], 2010, "07/20/2018"));
-//console.log(carro(2, "Ka", "Palio", "Verde", ["Direcao hidraulica", "roda liga leve", "manual"]));
-//console.log(carro(3, "Corsa", "Chevrolet", "Prata", ["alarme", "travas"]));
+//console.log(carro(1, "Ka", "Ford", "Vermelho", ["vidro eletrico", "teto-solar", "automatico"], 25000, 2010, "08/20/2010", "07/20/2018"));
+//console.log(carro(2, "Ka", "Palio", "Verde", ["Direcao hidraulica", "roda liga leve", "manual"], 30000, 2015, "12/01/2015", "05/20/2018"));
+console.log(carro(3, "Corsa", "Chevrolet", "Prata", ["alarme", "travas"], 12000, 2018, "01/01/2018", "07/12/2018"));
 console.log("===================");
 console.log("Lista de Atributos");
 console.log("===================");
@@ -90,6 +109,17 @@ console.log("========================");
 console.log("Tempo para o vencimento");
 console.log("========================");
 tempoVencimento(carro.vencimentoDoc);
+console.log("");
+console.log("==============================");
+console.log("Valor proporcional Depreciado");
+console.log("==============================");
+valorDepreciado(carro);
+console.log("");
+console.log("========================");
+console.log("Verifica Teto-Solar");
+console.log("========================");
+verificaAcessorio("teto-solar");
+
 
 
 
