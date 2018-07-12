6877e1e509957eb454cc832626d6d24b77658a75
Mazzuco <Gustavo.Mazzuco@thomsonreuters.com>
2018-07-12 14:02:08 -0300

    Modificação 1 de testeJS

--- testeJS.js
+++ testeJS.js
@@ -1,4 +1,4 @@
-var carro = function (id, nome, marca, cor, acessorios, ano) {
+var carro = function (id, nome, marca, cor, acessorios, ano, vencimentoDoc) {
 
     return carro = {
         id: id,
@@ -6,7 +6,8 @@ var carro = function (id, nome, marca, cor, acessorios, ano) {
         marca: marca,
         cor: cor,
         acessorios: acessorios,
-        ano: ano
+        ano: ano,
+        vencimentoDoc: vencimentoDoc
     }
 
 };
@@ -30,19 +31,32 @@ var consumoCarro = function (quilometros, litros){
     }
 };
 
-var verificarDocumento = function (dataVencimento){
+var validadeDocumento = function (dataVencimento){
     
-    var hoje = new Date().toDateString();
-    var vencimento = new Date(dataVencimento).toDateString();
+    var hoje = new Date();
+    var vencimento = new Date(dataVencimento);
 
     if (vencimento < hoje){
-        console.log("Documento está vencido");
+        console.log("Documento está vencido.");
     } else if (vencimento === hoje ){
-        console.log("Documento vence hoje");
+        console.log("Documento vence hoje.");
     } else {
-        console.log("Documento está em dia");
+        console.log("Documento está em dia.");
     }
 };
+
+var tempoVencimento = function (dataVencimento){
+    var hoje = new Date();
+    var vencimento = new Date(dataVencimento);
+
+    if (hoje <= vencimento){
+        var diferenca = Math.abs(vencimento.getTime() - hoje.getTime());
+        var diferencaDias = Math.ceil(diferenca / (1000 * 3600 * 24));
+        console.log("Faltam "+ diferencaDias + " dias para o documento vencer.");
+    } else {
+        console.log("O documento já está vencido");
+    }
+}
 
 var verificaTetoSolar = function(acessorio){
     var carroAcessorio = carro.acessorios.filter(function (elemento){
@@ -54,18 +68,28 @@ var verificaTetoSolar = function(acessorio){
 //    return elemento === "automatico";
 //});
 
-console.log(carro(1, "Ka", "Ford", "Vermelho", ["vidro eletrico", "Teto Solar", "automatico"], 2010));
+console.log(carro(1, "Ka", "Ford", "Vermelho", ["vidro eletrico", "Teto Solar", "automatico"], 2010, "07/20/2018"));
 //console.log(carro(2, "Ka", "Palio", "Verde", ["Direcao hidraulica", "roda liga leve", "manual"]));
 //console.log(carro(3, "Corsa", "Chevrolet", "Prata", ["alarme", "travas"]));
-
-console.log("");
+console.log("===================");
+console.log("Lista de Atributos");
+console.log("===================");
 listarAtributos(carro);
 console.log("");
-consumoCarro(100, 10);
+console.log("===================");
+console.log("Consumo médio");
+console.log("===================");
+consumoCarro(100, 5);
 console.log("");
-verificarDocumento('07/07/2018');
+console.log("=========================");
+console.log("Verificação do vencimento");
+console.log("=========================");
+validadeDocumento(carro.vencimentoDoc);
 console.log("");
-verificaTetoSolar("Teto Solar");
+console.log("========================");
+console.log("Tempo para o vencimento");
+console.log("========================");
+tempoVencimento(carro.vencimentoDoc);
 
 
 
